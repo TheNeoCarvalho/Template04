5e2a425512b96de8dd1c027363600b71bedeca0a
Manoel Carvalho <manoelmecat@gmail.com>
2018-10-26 11:37:39 -0300

    Template Finalizado

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+$(function(){
+
+	$('.nav-toggle, .nav-close').click(function(e){
+		e.preventDefault();
+		$('.nav').toggleClass('active');
+	});
+
+	$(window).scroll(function(){
+		if($(this).scrollTop() > 100){
+			$('.header').addClass('fixed');
+		}else{
+			$('.header').removeClass('fixed');
+		}
+	});
+
+});
